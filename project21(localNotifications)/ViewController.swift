199f1fa4d983ba1b92c58ab4e9f2d8d944094c11
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-07-20 13:13:04 +0300

    Initial Commit

---
+++ project21(localNotifications)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project21(localNotifications)
+//
+//  Created by Nikita  on 7/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
